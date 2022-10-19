c419aacbde004c60d428e90f90d71a192e339908
Abdulmenam <93938541+Abdulmenam@users.noreply.github.com>
2022-10-19 13:07:24 +0300

    submit the task

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,39 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+private String color;
+	private int numberOfPaws;
+	private boolean hasFur;
+	
+	public Animal(String color,int numberOfPaws,boolean hasFur) {  //create constructor , could have input parameters called "parameterised".
+		this.color= color;
+		this.numberOfPaws= numberOfPaws;
+		this.hasFur= hasFur;//(the parameter); //this before name of class field.
+	}
+	public String getColor() {  
+		return color;
+	}
+	public int getPaws() {
+		return numberOfPaws;
+	}
+	public boolean getHasfur() { 
+		return hasFur;
+	}
+	
+	public String getDescription() {
+		 String description; 
+		 if(numberOfPaws!=1) {
+			 description = hasFur? "This animal is mostly " +color+". It has " + numberOfPaws + " paws and a fur." 
+	                    : "This animal is mostly " +color+". It has " + numberOfPaws + " paws and no fur.";
+		 }
+			 else {description = hasFur? "This animal is mostly " +color+". It has " + numberOfPaws + " paw and a fur." 
+                 : "This animal is mostly " +color+". It has " + numberOfPaws + " paw and no fur.";
+			 }
+		return  description;
+				
+		
+	}
 
+	
 }
+
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,27 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+	public Bird() {
+		super("blue", 2, false);
 
+	}
+	public String getDescription() {
+		 String description; 
+		 
+		 
+		 if(getPaws()!=1) {
+			 description = getHasfur()? "This animal is mostly " +getColor()+". It has " + getPaws() + " paws and a fur. "
+			 		+ "Moreover, it has " + getPaws() + " wings and can fly." 
+	                    : "This animal is mostly " +getColor()+". It has " + getPaws() + " paws and no fur. "
+	                    		+ "Moreover, it has " + getPaws() + " wings and can fly.";
+		 }
+			 else {description = getHasfur()? "This animal is mostly " +getColor()+". It has " + getPaws() + " paw and a fur. "
+			 		+ "Moreover, it has " + getPaws() + " wings and can fly." 
+                : "This animal is mostly " +getColor()+". It has " + getPaws() + " paw and no fur. "
+                		+ "Moreover, it has " + getPaws() + " wings and can fly.";
+			 }
+		return  description; 
+				
+		
+	}
 }
